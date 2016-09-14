86a8e4f3ea443d940ec5ca48e897bdecfe142e0c
LiuQiang <forxfor@163.com>
2016-09-14 17:10:37 +0800

    1.1完成单个目录下都是数据文件, 加载文件数据到数据库中

--- MdUtil/main.cpp
+++ MdUtil/main.cpp
@@ -1,11 +1,14 @@
 ﻿#include <QCoreApplication>
+#include <QDirIterator>
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QDateTime>
 #include <string.h>
 #include <QDebug>
 #include <QFile>
+#include <QDir>
 
+#include <QDate>
 
 int main(int argc, char *argv[])
 {
@@ -20,50 +23,64 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    QFile file("data_res/000001_20160104.csv");
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
-        qDebug() << "open file error" << endl;
-        return -2;
-    }
-
-    file.readLine();
-
-    char *sql = new char[5120];
-    int flag = 0;
-    while(!file.atEnd()){
-        QByteArray line = file.readLine();
-        QStringList l = QString(line).split(',');
-        // 如果 l 的大小等于28 的时候继续执行, 如果不等于28的时候,则输出 l.size() == 28;
-        Q_ASSERT(l.size() == 28);
-        QDateTime stamp = QDateTime::fromString(l.at(2), "yyyy-MM-dd HH:mm:ss");
-
-        if (!flag){
-            //  插入的insert 头部分
-            sprintf(sql, "insert into md values(%d,%d,'%.4f')",
-                    l.at(1).toInt(),
-                    stamp.toTime_t(),
-                    l.at(3).toDouble()
-                    );
-            flag = 1;
+    QDir res_dir("./data_res/real_data/");
+    QDirIterator itr(res_dir);
+    int i = 0;
+    while(itr.hasNext()){
+        QString file_path = itr.next();
+        qDebug() << file_path << endl;
+        if ( file_path == "./data_res/real_data/." || file_path == "./data_res/real_data/.."){
+            qDebug() << "continue" << endl;
             continue;
         }
-        // 临时 buffer
-        char tmp_sql[50];
-        sprintf(tmp_sql, ",(%d,%d,'%.4f')",
-                    l.at(1).toInt(),
-                    stamp.toTime_t(),
-                    l.at(3).toDouble()
-                );
-        // 拼接每次的 sql 插入的内容
-        strcat(sql, tmp_sql);
+        QFile file(file_path);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+            qDebug() << "open file error" << endl;
+            return -2;
+        }
+
+        file.readLine();
+        QString *sql = new QString;
+        int flag = 0;
+
+        while(!file.atEnd()){
+            QByteArray line = file.readLine();
+            QStringList l = QString(line).split(',');
+            // 如果 l 的大小等于28 的时候继续执行, 如果不等于28的时候,则输出 l.size() == 28;
+            Q_ASSERT(l.size() == 28);
+            QDateTime stamp = QDateTime::fromString(l.at(2), "yyyy-MM-dd HH:mm:ss");
+
+            if (!flag){
+                //  插入的insert 头部分
+                sql->sprintf("insert into md values(%d,%d,'%.4f')",
+                        l.at(1).toInt(),
+                        stamp.toTime_t(),
+                        l.at(3).toDouble()
+                        );
+                flag = 1;
+                continue;
+            }
+            // 临时 buffer
+            QString part_sql;
+            part_sql.sprintf(",(%d,%d,'%.4f')",
+                        l.at(1).toInt(),
+                        stamp.toTime_t(),
+                        l.at(3).toDouble()
+                    );
+            // 拼接每次的 sql 插入的内容
+            sql->append(part_sql);
+        }
+
+        // 执行语句
+        qDebug() << "插入..... 啊" << i++ << endl;
+        db.exec(*sql);
+        qDebug() << "完成.." << endl;
+        file.close();
+        delete sql;
+
     }
 
-    db.exec(sql);
-    delete []sql;
     db.close();
-    file.close();
-
-
 
     return 0;
 }
